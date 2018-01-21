15cd415fc15230d63d9a0f9f310f23d2d1a35685
Hernan Cabrera <mauricioh_cabrera@hotmail.com>
2018-01-21 17:24:13 -0300

    Solo controlador de hechizos

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -9,21 +9,21 @@
         :class="{ 'esta-activo': opcion === 1 }"
         to="/"
       )
-      router-link.item.icon-leaf(
-        @click="cambiarAPP(2)"
-        :class="{ 'esta-activo': opcion === 2 }"
-        to="jungla"
-      )
-      router-link.item.icon-man(
-        @click="cambiarAPP(3)"
-        :class="{ 'esta-activo': opcion === 3 }"
-        to="redes-sociales"
-      )
-      router-link.item.icon-cog(
-        @click="cambiarAPP(4)"
-        :class="{ 'esta-activo': opcion === 4 }"
-        to="ajustes"
-      )
+      //- router-link.item.icon-leaf(
+      //-   @click="cambiarAPP(2)"
+      //-   :class="{ 'esta-activo': opcion === 2 }"
+      //-   to="jungla"
+      //- )
+      //- router-link.item.icon-man(
+      //-   @click="cambiarAPP(3)"
+      //-   :class="{ 'esta-activo': opcion === 3 }"
+      //-   to="redes-sociales"
+      //- )
+      //- router-link.item.icon-cog(
+      //-   @click="cambiarAPP(4)"
+      //-   :class="{ 'esta-activo': opcion === 4 }"
+      //-   to="ajustes"
+      //- )
 </template>
 
 <script>
